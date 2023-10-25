12628bef1b1ec0f1e875fb51dcf557f0be6e1957
Victor <j1747victor@gmail.com>
2023-10-25 10:08:25 -0300

    implementando_luz

---
+++ Assets/rotation.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour
+{
+   
+    public float rotationSpeed;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+
+    }
+}
