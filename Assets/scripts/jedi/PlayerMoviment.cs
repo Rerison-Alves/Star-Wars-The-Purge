53f805898dce5d08c332f5e0f58243d06d33c3a4
Victor <j1747victor@gmail.com>
2023-10-25 19:54:44 -0300

    implementando animação na movimentação

--- Assets/scripts/jedi/PlayerMoviment.cs
+++ Assets/scripts/jedi/PlayerMoviment.cs
@@ -6,13 +6,18 @@ public class PlayerMoviment : MonoBehaviour
 {
     public float velocidadeMovi = 5f;
     public Rigidbody2D rb;
+    public Animator animator;
     Vector2 movement;
+
     // Update is called once per frame
     void Update()
     {
         //input
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate()
