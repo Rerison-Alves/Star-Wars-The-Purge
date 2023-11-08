bee78e72b362b5e5b590d9d9f7dd4f0faed32eff
Rerison-Alves <107881260+Rerison-Alves@users.noreply.github.com>
2023-11-08 18:08:19 -0300

    Squashed commit of the following:
    commit cbc4b82a240ba557e7cb9ece4552178606272bf9
    Author: Rerison-Alves <107881260+Rerison-Alves@users.noreply.github.com>
    Date:   Wed Nov 8 18:06:05 2023 -0300
    
        Animações de movimentação e de combat
    
        Novas animações de movimentação e de combate implementadas

--- Assets/scripts/jedi/PlayerMoviment.cs
+++ Assets/scripts/jedi/PlayerMoviment.cs
@@ -1,28 +1,103 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMoviment : MonoBehaviour
 {
+    // Movimentação
     public float velocidadeMovi = 5f;
     public Rigidbody2D rb;
-    public Animator animator;
     Vector2 movement;
 
+    // Animação
+    public SpriteRenderer sprite;
+    public Animator animator;
+    public float idleHorizontal = 0;
+    public float idleVertical = 0;
+    
+
+    // Attack
+    public float delay = 0.3f;
+    public bool attackblock;
+
+
+
+    private void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        animator=GetComponent<Animator>();
+    }
     // Update is called once per frame
     void Update()
     {
-        //input
+        
+        // input
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        // Animator
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+        Flip(movement.x);
+
+        SetIdle();
+        animator.SetFloat("IdleHorizontal", idleHorizontal);
+        animator.SetFloat("IdleVertical", idleVertical);
+
+        Attack();
     }
 
     void FixedUpdate()
     {
         //movimentação
         rb.MovePosition(rb.position + movement * velocidadeMovi * Time.fixedDeltaTime);
+    }
+
+
+    public void Flip(float horizontal)
+    {
+        if (horizontal>=0 && idleHorizontal>=0)
+        {
+            sprite.flipX = false;
+        }
+        else
+        {
+            sprite.flipX= true;
+        }
+    }
+
+    public void SetIdle()
+    {
+        if (movement.sqrMagnitude != 0)
+        {
+            if (movement.x != 0)
+            {
+                idleHorizontal = movement.x;
+                idleVertical = 0;
+            }
+            if (movement.y != 0)
+            {
+                idleVertical = movement.y;
+                idleHorizontal = 0;
+            }
+        }
+    }
+    public void Attack()
+    {
+        if (Input.GetMouseButtonDown(0)) 
+        { 
+            if (attackblock) return;
+            animator.SetTrigger("Attack");
+            attackblock = true;
+            StartCoroutine(DelayAttacked());
+        }
+    }
+
+    private IEnumerator DelayAttacked()
+    {
+        yield return new WaitForSeconds(delay);
+        attackblock = false;
     }
 }
