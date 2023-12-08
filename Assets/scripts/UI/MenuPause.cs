957d0a3d318168866e40e3ccf5f1636b61d22271
Israel Marques de Oliveira <israelmarques007@gmail.com>
2023-12-08 13:29:19 -0300

    Conserto Bug Sair-Menu

--- Assets/scripts/UI/MenuPause.cs
+++ Assets/scripts/UI/MenuPause.cs
@@ -9,10 +9,13 @@ public class MenuPause : MonoBehaviour
     public string nomeMenu;
     public string cenaName;
 
+    // Flag para verificar se o som estava ativo antes de pausar o jogo
+    private bool isAudioEnabled = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -35,6 +38,9 @@ public class MenuPause : MonoBehaviour
     {
         pauseMenu.gameObject.SetActive(true);
         Time.timeScale = 0;
+
+        // Salva o estado atual do AudioListener
+        isAudioEnabled = AudioListener.pause;
         AudioListener.pause = true; // Pausa todos os sons do jogo
     }
 
@@ -42,20 +48,36 @@ public class MenuPause : MonoBehaviour
     {
         pauseMenu.gameObject.SetActive(false);
         Time.timeScale = 1;
-        AudioListener.pause = false; // Continua todos os sons do jogo
+
+        // Restaura o estado do AudioListener
+        AudioListener.pause = isAudioEnabled;
     }
 
     public void Continue()
     {
         ContinueGame();
     }
+
     public void Sair()
     {
+        Time.timeScale = 1;
+
+        // Reseta o AudioListener antes de ir para o menu
+        ResetAudioListener();
+
         SceneManager.LoadScene(nomeMenu);
     }
+
     public void Reiniciar()
     {
         SceneManager.LoadScene(cenaName);
         ContinueGame();
+    }
+
+    // Método para resetar o AudioListener
+    private void ResetAudioListener()
+    {
+        // Garante que o AudioListener está ativado ao sair
+        AudioListener.pause = false;
     }
 }
